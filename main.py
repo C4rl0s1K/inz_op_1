564211392b95f52fd6586a04aa2935a83debf5ca
Anita Carlos <anita.carlos@student.ue.poznan.pl>
2024-03-24 11:05:14 +0100

    Policzyłam pole trojkata

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ print(pole_kwadratu)
 bok_b = 3
 bok_c = 5
 pole_prostokata = bok_b * bok_c
+
 print(pole_prostokata)
 
+#Pole trąjkąta
+podst = 5
+h = 6
+pole_trojkata = (podst * h)/2
 
-
+print(pole_trojkata)
 
